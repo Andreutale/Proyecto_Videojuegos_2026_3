da9ede5c3d7775c910885172366eaecd197c871a
juliabeltr <jbelgir@upv.edu.es>
2026-05-19 17:04:27 +0200

    llave animacion

--- Assets/Animations/KeyAnimation.cs
+++ Assets/Animations/KeyAnimation.cs
@@ -2,9 +2,9 @@ using UnityEngine;
 
 public class KeyAnimation : MonoBehaviour
 {
-    public float rotationSpeed = 80f;
+    public float rotationSpeed = 45f;
     public float floatSpeed = 2f;
-    public float floatHeight = 0.15f;
+    public float floatHeight = 0.1f;
 
     private Vector3 startPos;
 
@@ -16,7 +16,7 @@ public class KeyAnimation : MonoBehaviour
     void Update()
     {
         // Cambia el eje aquí
-        transform.Rotate(Vector3.forward * rotationSpeed * Time.deltaTime);
+        transform.Rotate(0f, rotationSpeed * Time.deltaTime, 0f, Space.Self);
 
         float newY = startPos.y + Mathf.Sin(Time.time * floatSpeed) * floatHeight;
 
